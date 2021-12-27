d59a8fd2dd25d255786d5f4f52bac2b3db62a90d
Seyyit atım <seyyit.atim@ogr.sakarya.edu.tr>
2021-12-27 23:13:16 +0300

    authorize

--- Mobi/Controllers/WebGLController.cs
+++ Mobi/Controllers/WebGLController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mobi.Controllers
 {
     public class WebGLController : Controller
     {
+        [Authorize]
         public IActionResult Index()
         {
             return View();
